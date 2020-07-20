85fa8f6d3461bac00e5accb3d50a0acf1dc93a2d
Chana Kupetz <chanakupetz@gmail.com>
2020-07-20 05:10:22 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -14,6 +14,10 @@ def link
 end
 
 def all_phrases
-  all_phrases = " #{status} #{phrase} #{phrase1}"
-  puts all_phrases
+  phrase = "It's-a me, Mario!"
+  puts phrase
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts status
+  phrase1 = "It's Dangerous To Go Alone! Take This."
+  puts phrase1
 end
